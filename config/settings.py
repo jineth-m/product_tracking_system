4d5ba5463e42af08eaacde1071b8ba3ede443b21
jineth-m <morawakajb.22@uom.lk>
2026-02-02 20:23:40 +0530

    database migrations

--- config/settings.py
+++ config/settings.py
@@ -65,6 +65,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -106,14 +107,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'product_tracking_db',
-        'USER': 'postgres',
-        'PASSWORD': 'AbCd3142',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 
@@ -161,3 +159,5 @@ LOGIN_URL = '/login/'
 TIME_ZONE = "Asia/Colombo"
 USE_TZ = True
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
