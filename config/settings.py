c06720fd5283c1deb0ef008b73616426c17925e8
jineth-m <morawakajb.22@uom.lk>
2026-02-09 07:32:35 +0530

    debug database config

--- config/settings.py
+++ config/settings.py
@@ -108,9 +108,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default="sqlite:///db.sqlite3"
-    )
+    "default": dj_database_url.config(default="sqlite:///db.sqlite3")
 }
 
 
